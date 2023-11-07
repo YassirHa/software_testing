fcef52863b66c647e1566b92d0faa4fca7f93c8a
Reda Mettioui <Reda.mettioui@student.odisee.be>
2023-11-07 14:49:05 +0100

    Update

---
+++ tasklist/src/Pages/AnimeList.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [filter, setFilter] = useState('All');
+  const [items, setItems] = useState([]);
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
+  useEffect(() => {
+    const fetchDataWithRateLimit = async () => {
+      const itemIds = Array.from({ length: 20 }, (_, index) => index + 1);
+
+      const maxConcurrentRequests = 5;
+
+      const itemPromises = itemIds.map(async (itemId, index) => {
+        if (index % maxConcurrentRequests === 0) {
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+        }
+
+        try {
+          const response = await fetch(`https://api.jikan.moe/v4/anime/${itemId}`);
+          const data = await response.json();
+          if (data.data && data.data.mal_id) {
+            const itemData = {
+              id: data.data.mal_id,
+              title: data.data.title,
+              status: 'Current',
+              imageUrl: data.data.images.jpg,
+              score: 'None',
+              episodes: data.data.episodes,
+            };
+            return itemData;
+          }
+        } catch (error) {
+          console.error('Error fetching data for item', itemId, error);
+        }
+        return null;
+      });
+
+      const itemData = (await Promise.all(itemPromises)).filter((item) => item !== null);
+      setItems(itemData);
+    };
+
+    fetchDataWithRateLimit();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner banner-image">
+      </div>
+
+      <div className="menu">
+        <button onClick={() => handleFilterChange('All')}>All</button>
+        <button onClick={() => handleFilterChange('Current')}>Current</button>
+        <button onClick={() => handleFilterChange('Completed')}>Completed</button>
+        <button onClick={() => handleFilterChange('Paused')}>Paused</button>
+        <button onClick={() => handleFilterChange('Dropped')}>Dropped</button>
+        <button onClick={() => handleFilterChange('Planned')}>Planned</button>
+      </div>
+
+      <ul className="list">
+        {items.map((item) => {
+          if (filter === 'All' || filter === item.status) {
+            const backgroundStyle = {
+              backgroundImage: `url(${item.imageUrl})`,
+            };
+
+            return (
+              <li key={item.id} className="list-item" style={backgroundStyle}>
+                <div className="text">
+                  <p className="title">{item.title}</p>
+                  <p className="score">{item.score}</p>
+                  <p className="episodes">{`${item.episodes}Ep`}</p>
+                </div>
+              </li>
+            );
+          }
+          return null;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
